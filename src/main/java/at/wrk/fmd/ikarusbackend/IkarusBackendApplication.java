840dde7a2be0df69bdc0ee5c715898813e8c6f1d
MarkusWME <winter.markus@gmx.at>
2019-04-30 11:59:03 +0200

    FMD-75 Prepare Spring Boot project

---
+++ src/main/java/at/wrk/fmd/ikarusbackend/IkarusBackendApplication.java
@@ -0,0 +1,13 @@
+package at.wrk.fmd.ikarusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkarusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IkarusBackendApplication.class, args);
+    }
+
+}
