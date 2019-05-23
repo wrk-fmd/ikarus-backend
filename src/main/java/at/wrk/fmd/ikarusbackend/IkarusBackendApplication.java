f3a49f2ae8c7e74b65a39814f0fe4610b78bf740
Robert Wittek <r.m.wittek@gmail.com>
2019-05-23 21:50:51 +0200

    FMD-36 add war deployment support for tomcat

--- src/main/java/at/wrk/fmd/ikarusbackend/IkarusBackendApplication.java
+++ src/main/java/at/wrk/fmd/ikarusbackend/IkarusBackendApplication.java
@@ -2,9 +2,16 @@ package at.wrk.fmd.ikarusbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IkarusBackendApplication {
+public class IkarusBackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+        return builder.sources(IkarusBackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IkarusBackendApplication.class, args);
