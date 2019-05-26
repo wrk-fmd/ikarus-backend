ca4ab203b219c73281a66a47d451767bbfc60614
MarkusWME <winter.markus@gmx.at>
2019-05-27 01:07:43 +0200

    Add UI fixes for better touch usability

--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -44,14 +44,31 @@ function convertFormToJSON(formData) {
     return JSON.stringify(json);
 }
 
+function getStatusOrder(status) {
+    switch (status) {
+        case "NOT_REGISTERED": return 1;
+        case "REGISTERED": return 2;
+        case "REGISTERED_WITH_MATERIAL": return 3;
+        case "DEREGISTERED": return 4;
+        default: 0;
+    }
+}
+
 function getStatusString(status) {
     switch (status) {
-        case "NOT_REGISTERED": return "Nicht registriert";
-        case "REGISTERED": return "Registriert";
-        case "REGISTERED_WITH_MATERIAL": return "Registriert - Material ausgefasst";
-        case "DEREGISTERED": return "Abgemeldet";
+        case "NOT_REGISTERED": return "<i class='fas fa-times-circle text-danger' title='Nicht registriert'></i><span class='d-none text-danger status-text'><br/>Nicht registriert</span>";
+        case "REGISTERED": return "<i class='fas fa-check-circle text-success' title='Registriert'></i><span class='d-none text-success status-text'><br/>Registriert</span>";
+        case "REGISTERED_WITH_MATERIAL": return "<i class='fas fa-check-circle text-success' title='Registriert'></i>&nbsp;<i class='fas fa-box text-primary' title='Material ausgefasst'></i><span class='d-none text-primary status-text'><br/>Mit Materialausgabe registriert</span>";
+        case "DEREGISTERED": return "<i class='fas fa-walking text-secondary' title='Abgemeldet'></i><span class='d-none text-secondary status-text'><br/>Abgemeldet</span>";
         default: return status;
     }
+}
+
+function showStatusString(element) {
+    element.removeClass("d-none");
+    element.fadeIn(500, function() {
+        element.fadeOut(1500);
+    });
 }
 
 function getStatusColor(status) {
