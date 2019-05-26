e52e9236e66c2d3a239fe79545d337625274c70c
Robert Wittek <r.m.wittek@gmail.com>
2019-05-26 20:35:18 +0200

    RELEASE: 1.0.0

--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("body").append("<div id='info' class='modal'><div class='modal-dialog'><div class='modal-content'>" +
         "<div class='modal-header'><h4 class='modal-title'>Ikarus Info</h4><button class='close' data-dismiss='modal'>&times;</button></div>" +
-        "<div class='modal-body'>Ikarus Backend 1.0.0<br/><br/>Benutzte Drittanbieter-Bibliotheken: " +
+        "<div class='modal-body'>Ikarus Backend 1.0.1-SNAPSHOT<br/><br/>Benutzte Drittanbieter-Bibliotheken: " +
         "<a href='https://jquery.com/'>jQuery</a>, <a href='https://getbootstrap.com/'>Bootstrap</a>, <a href='https://datatables.net/'>DataTables</a> und <a href='https://fontawesome.com/'>Font Awesome</a></div>" +
         "<div class='modal-footer'><button class='btn btn-primary' data-dismiss='modal'>Schlie&szlig;en</button></div></div></div></div>");
 });
